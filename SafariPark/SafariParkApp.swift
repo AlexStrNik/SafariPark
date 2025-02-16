1f0cfb90d597db39bb15e607354cf414776c7ef8
alexstrnik <alex.str.nik@gmail.com>
2025-02-16 13:18:33 +0300

    Initial Commit

---
+++ SafariPark/SafariParkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafariParkApp.swift
+//  SafariPark
+//
+//  Created by Aleksandr Strizhnev on 16.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct SafariParkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
